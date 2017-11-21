30039ba5d76fb0bc483dc787793ba57b03489b61
ia-lsinclair <32363944+ia-lsinclair@users.noreply.github.com>
2017-11-21 11:41:05 -0500

    Password checker V2

--- c1_7_favorite_color.py
+++ c1_7_favorite_color.py
@@ -1,5 +1,35 @@
-# Name
-# Date
-# Python Beginnings
-# 7 Favorite Color
+#Mahit
+#11/21/17
+#Kim Jong Un simulator
 
+
+
+
+print("""Welcome to the duck blow up simulator!
+You have to tell me all the passwords to deactivate the Hydrolic Bomb.""")
+
+
+password1 = "ducks"
+password2 = "car radio"
+password3 = "Kim"
+password4 = "Jong"
+password5 = "Un"
+
+pwd1 = input("please type password one:")
+if pwd1 == password1:
+    print("You Can Pass to the next password step")
+    pwd1 = input("please type password one:")
+else:
+    print("You blew up and died")
+
+if pwdl == password2:
+    print(" Please move on")
+    pwdl = ("You can pass to the next password step")
+else:
+    print("""Self destruct in 3... 2... 1...
+             (  )  
+            (    )
+            |    |
+            |    |
+            ^^^^^^^^^^^^^^^^""")
+    
